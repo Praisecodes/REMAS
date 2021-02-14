8de8a8682dd7b005a7025e50425ae29ad95ae036
Praise Codes <73062242+Praisecodes@users.noreply.github.com>
2021-02-14 21:44:25 +0100

    Add files via upload

---
+++ WinBuCom.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Calculator.h"
+
+HWND buttonClear, buttonTotal, buttonPurchase, buttonMenu, hold, holder, holds, hold1, hold2, hold3;
+HWND TotalSign, SubTotalSign, totalSpace, subTotalSpace;
+HWND Pick1, Pick2, Pick3, Pick4, Pick5, Pick6;
+HWND Amount1, Amount2, Amount3, Amount4, Amount5, Amount6, Amount7;
+
+void Total()
+{
+	MessageBox(NULL, L"No Functionality Given Yet", L"TOTAL", MB_OK | MB_ICONEXCLAMATION);
+}
+
+void Purchase()
+{
+	MessageBox(NULL, L"No Functionality Given Yet", L"PURCHASE", MB_OK | MB_ICONEXCLAMATION);
+}
+
+void Clear()
+{
+	MessageBox(NULL, L"No Functionality Given Yet", L"CLEAR", MB_OK | MB_ICONEXCLAMATION);
+}
+
+void Menu()
+{
+	MessageBox(NULL, L"No Functionality Given Yet", L"MENU", MB_OK | MB_ICONEXCLAMATION);
+}
